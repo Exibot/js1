51c99c0311436ec08a775ec9e823401752b2629d
Enrique Altamirano <enrique.altamiros@gmail.com>
2023-01-05 14:57:54 -0300

    Ejercicio dos

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo")
+console.log(typeof 22.3)
+
+let objeto = {
+    a: 1,
+    b: 2,
+    c: 3,
+}
+
+let lista = [1, 2, 3]
+let lista2 = ['a', 'b', 'c']
+
+console.log(lista2[0])
